b76682afacd2760d672b2b90799cbab9882e39b0
LUIGI-PC\Luigi <pieczarka51@gmail.com>
2022-03-14 16:24:52 +0100

    API and API clients created

---
+++ APIClient/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var httpClinet = new HttpClient();
+
+httpClinet.BaseAddress = new Uri("http://localhost:5225/api/Articles/create");
+var i = 0;
+var max = 300;
+
+while (i < max)
+{
+    var response = await httpClinet.GetAsync("http://localhost:5225/api/Articles/create");
+    var result = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(result);
+
+    Thread.Sleep(1000);
+    i++;
+}
---
+++ ArticlesAPI/Controllers/ArticlesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticlesAPI.Controllers
+{
+    class Result
+    {
+        static int i =0;
+
+        public int Value
+        {
+            get 
+            {
+                i++;
+                return i; 
+            }
+        }
+
+    }
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        static bool isOk = true;
+        public ArticlesController()
+        {
+            result = new Result();
+        }
+        private Result result;
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            isOk = !isOk;
+            return Ok();
+        }
+
+        // GET: ArticlesController/Create
+        [HttpGet]
+        public ActionResult<int> Create()
+        {
+            if (isOk)
+            {
+                return Created("article", result.Value);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
